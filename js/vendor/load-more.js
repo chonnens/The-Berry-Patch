195614750d1244760261c9e58c9a78f1f3b3c9ca
chonnens <chelseahonnens@gmail.com>
2015-09-09 14:59:30 -0500

    Saved Photos as SVG

---
+++ js/vendor/load-more.js
@@ -0,0 +1,9 @@
+$(function(){
+    $("#col3").slice(0, 10).show(); // select the first ten
+    $("#load").click(function(e){ // click event for load more
+        e.preventDefault();
+        $("div:hidden").slice(0, 10).show(); // select next 10 hidden divs and show them
+        if($("div:hidden").length == 0){ // check if any hidden divs still exist
+        }
+    });
+});// JavaScript Document
